abbff20bbf3f4f443300113ea2a8cea5eb62ce63
Marlene Ganslmeier <73480232+SportyScientist@users.noreply.github.com>
2024-10-30 15:48:36 +0100

    updated logic in categorisation to handle NAs

--- R/classifications.r
+++ R/classifications.r
@@ -1,31 +1,38 @@
 #' Determine glycemic categories
 #'
-#' As Defined by: 
+#' As Defined by:
 #' NGT: Fasting glucose < 100, 120min glucose < 140, hba1c < 5.7 (ADA)
 #' iIFG: Fasting glucose elevated
 #' iIGT: 120min glucose elevated
 #' IFG+IGT: Fasting and 120min glucose elevated
 #' DIA: Fasting glucose above 126 OR 120min glucose above 200 OR HBA1C above 6.5
-#' @param glucose_000 The fasting glucose value 
-#' @param glucose_120 The 120min glucose value 
+#' @param glucose_000 The fasting glucose value
+#' @param glucose_120 The 120min glucose value
 #' @param hba1c HbA1c value. Not required when system = WHO.
-#' @param system ADA or WHO. Defaults to ADA 
+#' @param system ADA or WHO. Defaults to ADA
 #' @param details Logical (TRUE/FALSE). Wether the function should return a detailed differentiation of prediabetes or just "PRE". Defaults to FALSE
 #' @param unit_glucose Unit of the glucose values, can be SI (mmol/l) or conventional (mg/dl). Defaults to conventional.
 #' @param unit_hba1c Unit of the hba1c values, can be SI (mmol/mol) or perc (%). Defaults to perc.
-#' 
+#'
 #' @return Glycemic Category
-#' @examples 
+#' @examples
 #' classify_glycemia(90, 160, 6.9, system = "ADA", details = TRUE)
 #' @export
-classify_glycemia <- function(glucose_000, glucose_120, hba1c = NULL, system = "ADA", 
-                              details = FALSE, 
+classify_glycemia <- function(glucose_000, glucose_120, hba1c = NULL, system = "ADA",
+                              details = FALSE,
                               unit_glucose = "conventional", unit_hba1c = "perc") {
+  validate_value <- function(value) {
+    if (is.na(value)) {
+      return(FALSE)
+    }
+    return(!is.numeric(value) || value < 0)
+  }
+
   # Input checks
-  if (!is.numeric(glucose_000) || glucose_000 <= 0) {
+  if (validate_value(glucose_000)) {
     stop("Error: 'glucose_000' must be a positive numeric value.")
   }
-  if (!is.numeric(glucose_120) || glucose_120 <= 0) {
+  if (validate_value(glucose_120)) {
     stop("Error: 'glucose_120' must be a positive numeric value.")
   }
   if (!system %in% c("ADA", "WHO")) {
@@ -46,38 +53,40 @@ classify_glycemia <- function(glucose_000, glucose_120, hba1c = NULL, system = "
   if (!unit_hba1c %in% c("perc", "SI")) {
     stop("Error: 'unit_hba1c' must be either 'perc' or 'SI'.")
   }
-  
+
   # Convert units if necessary
   if (unit_glucose == "SI") {
     glucose_000 <- glucose_000 * 18.016
     glucose_120 <- glucose_120 * 18.016
   }
+
   if (unit_hba1c == "SI" && !is.null(hba1c)) {
     hba1c <- (hba1c * 0.0915) + 2.15
   }
 
   # Initialize result
   result <- "Unknown"
-  
+
   # ADA Classification
   if (system == "ADA") {
-    if (!is.null(hba1c) && hba1c >= 6.5) {
+    if ((!is.na(hba1c) && hba1c >= 6.5) ||
+      (!is.na(glucose_000) && glucose_000 >= 126) ||
+      (!is.na(glucose_120) && glucose_120 >= 200)) {
       result <- "DIA"
-    } else if (glucose_000 >= 126 || glucose_120 >= 200) {
-      result <- "DIA"
-
-    } else if ((glucose_000 >= 100 && glucose_000 < 126) || 
-               (glucose_120 >= 140 && glucose_120 < 200) || 
-               (!is.null(hba1c) && hba1c >= 5.7 && hba1c < 6.5)) {
+    } else if (is.na(hba1c) || is.na(glucose_000) || is.na(glucose_120)) {
+      result <- NA
+    } else if ((glucose_000 >= 100 && glucose_000 < 126) ||
+      (glucose_120 >= 140 && glucose_120 < 200) ||
+      (hba1c >= 5.7 && hba1c < 6.5)) {
       if (details) {
-        if (glucose_000 >= 100 && glucose_000 < 126 && 
-            glucose_120 < 140) {
+        if (glucose_000 >= 100 && glucose_000 < 126 &&
+          glucose_120 < 140) {
           result <- "iIFG"
-        } else if (glucose_120 >= 140 && glucose_120 < 200 && 
-                   glucose_000 < 100) {
+        } else if (glucose_120 >= 140 && glucose_120 < 200 &&
+          glucose_000 < 100) {
           result <- "iIGT"
-        } else if (glucose_000 >= 100 && glucose_000 < 126 && 
-                   glucose_120 >= 140 && glucose_120 < 200) {
+        } else if (glucose_000 >= 100 && glucose_000 < 126 &&
+          glucose_120 >= 140 && glucose_120 < 200) {
           result <- "IFG+IGT"
         } else {
           result <- "PRE"
@@ -89,22 +98,25 @@ classify_glycemia <- function(glucose_000, glucose_120, hba1c = NULL, system = "
       result <- "NGT"
     }
   }
-  
+
   # WHO Classification
   if (system == "WHO") {
-    if (glucose_000 >= 126 || glucose_120 >= 200) {
+    if ((!is.na(glucose_000) && glucose_000 >= 126) ||
+      (!is.na(glucose_120) && glucose_120 >= 200)) {
       result <- "DIA"
-    } else if ((glucose_000 >= 110 && glucose_000 < 126) || 
-               (glucose_120 >= 140 && glucose_120 < 200)) {
+    } else if (is.na(glucose_000) || is.na(glucose_120)) {
+      result <- NA
+    } else if ((glucose_000 >= 110 && glucose_000 < 126) ||
+      (glucose_120 >= 140 && glucose_120 < 200)) {
       if (details) {
-        if (glucose_000 >= 110 && glucose_000 < 126 && 
-            glucose_120 < 140) {
+        if (glucose_000 >= 110 && glucose_000 < 126 &&
+          glucose_120 < 140) {
           result <- "iIFG"
-        } else if (glucose_120 >= 140 && glucose_120 < 200 && 
-                   glucose_000 < 110) {
+        } else if (glucose_120 >= 140 && glucose_120 < 200 &&
+          glucose_000 < 110) {
           result <- "iIGT"
-        } else if (glucose_000 >= 110 && glucose_000 < 126 && 
-                   glucose_120 >= 140 && glucose_120 < 200) {
+        } else if (glucose_000 >= 110 && glucose_000 < 126 &&
+          glucose_120 >= 140 && glucose_120 < 200) {
           result <- "IFG+IGT"
         } else {
           result <- "PRE"
@@ -116,7 +128,6 @@ classify_glycemia <- function(glucose_000, glucose_120, hba1c = NULL, system = "
       result <- "NGT"
     }
   }
-  
+
   return(result)
 }
-
--- utilities/update_package.r
+++ utilities/update_package.r
@@ -1,5 +1,3 @@
 library(roxygen2)
 roxygenise()
 
-library(devtools)
-install_github("SportyScientist/DiabUtils")
